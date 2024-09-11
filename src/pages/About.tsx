0fc1f49e85d366af865f18de749b7575e960c71c
elshora97 <A.elshora@dataserve.com.sa>
2024-09-12 02:37:46 +0300

    Add About Page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,19 @@
 function About() {
-  return <h1 className='text-4xl'>About Page</h1>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap items-center justify-center gap-2 sm:gap-x-6 text-4xl sm:text-6xl leading-none tracking-wide font-bold">
+        we love
+        <span className="bg-primary py-2 px-4 text-white rounded-lg tracking-widest">
+          NANA Store
+        </span>
+      </h1>
+      <p className="mt-6 text-ld leading-8 tracking-wide max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        molestias delectus voluptas perspiciatis ipsum animi enim saepe quam?
+        Expedita, odit officia. Nisi provident debitis odio, fuga suscipit quis
+        reprehenderit ipsa.
+      </p>
+    </section>
+  );
 }
 export default About;
